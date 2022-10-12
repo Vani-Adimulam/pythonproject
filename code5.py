f14d388ee0c7453df0f10bbd323adfe09bacf4ec
Vani-Adimulam <112381589+Vani-Adimulam@users.noreply.github.com>
2022-10-12 17:18:17 +0530

    Add files via upload

---
+++ code5.py
@@ -0,0 +1,45 @@
+def add(x,y):
+    return x+y
+def substract(x,y):
+    return x-y
+def multiply(x,y):
+    return x*y
+def divide(x,y):
+    return x/y
+
+while True:
+    choice = input("enter operator (add, substract, multiply, divide):")
+
+    if choice in ("add", "substract", "multiply", "divide"):
+        input1 = float(input("Enter input1:"))
+        input2 = float(input("Enter input2:"))
+
+        if choice == "add":
+            print(input1,"+",input2,"=",add(input1,input2))
+        elif choice == "substract":
+            print(input1,"-",input2,"=",substract(input1,input2))
+        elif choice == "multiply":
+            print(input1,"*",input2,"=",multiply(input1,input2))
+        elif choice == "divide":
+            print(input1,"/",input2,"=",divide(input1,input2))
+
+        next_calculation = input("Let's do next calculation? (continue/exit): ")
+        if next_calculation == "exit":
+            break
+
+""""
+        output:
+        enter
+        operator(add, substract, multiply, divide): add
+        Enter
+        input1: 4
+        Enter
+        input2: 5
+        4.0 + 5.0 = 9.0
+        Let
+        's do next calculation? (continue/exit): exit
+
+        Process
+        finished
+        with exit code 0
+"""
